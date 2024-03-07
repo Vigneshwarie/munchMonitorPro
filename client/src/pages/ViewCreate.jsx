cd1f1c2d0d057db1464fb92df7241f8343c9e322
rtocastro <ricktorres@live.com>
2024-03-07 00:01:35 -0800

    added a card to page

--- client/src/pages/ViewCreate.jsx
+++ client/src/pages/ViewCreate.jsx
@@ -1,6 +1,7 @@
 import createPetPro from '../assets/createpetprobutton.png'
 import { Card, Media, Content, Heading } from 'react-bulma-components'
 import Mypets from '../assets/mypets.png';
+import PetCard from '../components/PetCard'
 
 export default function ViewCreate() {
     return (
@@ -9,6 +10,8 @@ export default function ViewCreate() {
             <  img src={createPetPro} className="createbutton" />
             <div></div>
             < img src={Mypets} className="mypetspic" />
+
+            <PetCard />
 
 
         </>
