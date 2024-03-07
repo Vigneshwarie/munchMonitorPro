96bdaf7eb7620271a9feae65243af8bd66aeb0ab
rtocastro <ricktorres@live.com>
2024-03-06 21:14:34 -0800

    edited header, left only logo on

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import headerlogo from '../assets/mm2headerlogo.png'
 export default function Header(){
     return(
         <>
-        <h1>This is the header</h1>
+
         <  img src={headerlogo} className="headlogo" />
 
         </>
