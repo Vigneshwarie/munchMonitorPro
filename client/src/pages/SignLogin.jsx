18b2f55385c2405846c385bc8c5b93465ef0fbb9
rtocastro <ricktorres@live.com>
2024-03-08 00:19:45 -0800

    sign in/up page styling

--- client/src/pages/SignLogin.jsx
+++ client/src/pages/SignLogin.jsx
@@ -11,11 +11,19 @@ import Signinpic from '../assets/mmprosignin.png';
 import Signuppic from '../assets/mmprosignup.png';
 
 //  Field, Label, Control, Input
+const styles = {
+    signinbutton: {
+      background: "#E7D045",
+      color: "black",
+      border: "#E7D045"
+    }
+  };
 
+  
 export default function SignLogin() {
     const [userFormData, setUserFormData] = useState({
         first_name: '',
-        last_name:'',
+        last_name: '',
         email: '',
         password: '',
     });
@@ -61,7 +69,7 @@ export default function SignLogin() {
 
         setUserFormData({
             first_name: '',
-            last_name:'',
+            last_name: '',
             email: '',
             password: '',
         });
@@ -70,68 +78,110 @@ export default function SignLogin() {
 
     return (
         <>
-           <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-               {/* show alert if server response is bad */}
+            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                {/* show alert if server response is bad */}
                 <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-                        Something went wrong with your signup!
+                    Something went wrong with your signup!
                 </Alert>
 
-                    <Form.Group className='mb-3'>
-                        <Form.Label htmlFor='first_name'>First Name</Form.Label>
-                        <Form.Control
-                            type='text'
-                            placeholder='Enter your first name'
-                            name='first_name'
-                            onChange={handleInputChange}
-                            value={userFormData.first_name}
-                            required
-                        />
-                        <Form.Control.Feedback type='invalid'>First Name is required!</Form.Control.Feedback>
+                <img src={Signinpic} />
+
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Your email address'
+                        name='email'
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className='mb-3'>
-                        <Form.Label htmlFor='last_name'>Last Name</Form.Label>
-                        <Form.Control
-                            type='text'
-                            placeholder='Enter your last name'
-                            name='last_name'
-                            onChange={handleInputChange}
-                            value={userFormData.last_name}
-                            required
-                        />
-                        <Form.Control.Feedback type='invalid'>Last Name is required!</Form.Control.Feedback>
+                    <Form.Label htmlFor='password'>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                </Form.Group>
+
+                <Button
+                    disabled={!(userFormData.email && userFormData.password)}
+                    type='submit'
+                    style={ styles.signinbutton} className="signinbutton"
+                    variant='danger'>
+                    Sign In
+                </Button>
+
+                <br />
+
+
+                <img src={Signuppic} />
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='first_name'>First Name</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter your first name'
+                        name='first_name'
+                        onChange={handleInputChange}
+                        value={userFormData.first_name}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>First Name is required!</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className='mb-3'>
-                        <Form.Label htmlFor='email'>Email</Form.Label>
-                        <Form.Control
-                            type='email'
-                            placeholder='Your email address'
-                            name='email'
-                            onChange={handleInputChange}
-                            value={userFormData.email}
-                            required
-                        />
-                        <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                    <Form.Label htmlFor='last_name'>Last Name</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter your last name'
+                        name='last_name'
+                        onChange={handleInputChange}
+                        value={userFormData.last_name}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Last Name is required!</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className='mb-3'>
-                        <Form.Label htmlFor='password'>Password</Form.Label>
-                        <Form.Control
-                            type='password'
-                            placeholder='Your password'
-                            name='password'
-                            onChange={handleInputChange}
-                            value={userFormData.password}
-                            required
-                        />
-                        <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Your email address'
+                        name='email'
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='password'>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
                 </Form.Group>
                 <Button
-                        disabled={!(userFormData.first_name && userFormData.last_name && userFormData.email && userFormData.password)}
-                        type='submit'
-                        variant='success'>
-                        Submit
+                    disabled={!(userFormData.first_name && userFormData.last_name && userFormData.email && userFormData.password)}
+                    type='submit'
+                    variant='danger'>
+                    Sign Up!
                 </Button>
             </Form>
         </>
