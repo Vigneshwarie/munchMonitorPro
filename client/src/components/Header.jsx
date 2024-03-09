3b58d61e28755986c2ce57be192db2a12b8e4fc1
rtocastro <ricktorres@live.com>
2024-03-09 15:04:41 -0800

    added logoutbutton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import headerlogo from '../assets/mm2headerlogo.png'
+import { Button } from 'react-bootstrap';
 
-export default function Header(){
-    return(
+const styles = {
+    logoutbutton: {
+        background: "#81D8D0",
+        color: "white",
+        border: "#E7D045",
+        margin: "0, 0, 0, 0",
+
+
+    }
+};
+
+export default function Header() {
+    return (
         <>
+            <Button
+                // disabled={!(userFormData.petname && userFormData.petsex && userFormData.pettype && userFormData.petnotes)}
+                type='submit'
+                style={styles.logoutbutton} className="logoutbutton"
+            >
+                Logout
+            </Button>
 
-        <  img src={headerlogo} className="headlogo" />
+            <  img src={headerlogo} className="headlogo" />
 
         </>
     )
