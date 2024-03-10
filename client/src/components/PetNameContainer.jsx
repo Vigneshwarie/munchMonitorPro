8e61e220e825eac650721c3c06c5e616a83bf999
rtocastro <ricktorres@live.com>
2024-03-09 16:29:10 -0800

    added components for feeder page

--- client/src/components/PetNameContainer.jsx
+++ client/src/components/PetNameContainer.jsx
@@ -2,14 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function GreetingContainer() {
+function PetNameContainer() {
   return (
     <Container >
       <Row>
-        <Col> <strong>Greetings!</strong> **name here**</Col>
+        <Col> <strong>Pet Name!</strong> **name here**</Col>
       </Row>
     </Container>
   );
 }
 
-export default GreetingContainer;
+export default PetNameContainer;
