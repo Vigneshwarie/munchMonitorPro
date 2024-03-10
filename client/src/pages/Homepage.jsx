73a6944f3136e3af108b16980b421a9fed332d0c
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-03-09 23:05:32 -0500

    CreateProfile is renamed to Profile and modified, added separate CSS file, added link from homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,15 +1,19 @@
 import createPetPro from '../assets/createpetprobutton.png'
 import { Card, Media, Content, Heading } from 'react-bulma-components'
 import Mypets from '../assets/mypets.png';
-import PetCard from '../components/PetCard'
+import PetCard from '../components/PetCard';
+
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
     return (
         <>
-            <img src={createPetPro} className="createbutton" />
+            <Link to="/profile">
+                <img src={createPetPro} className="createbutton" />
+            </Link>
             <div></div>
             <img src={Mypets} className="mypetspic" />
             <PetCard />
         </>
-    )
+    );
 }
