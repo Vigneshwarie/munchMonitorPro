7a95874c066d1d8fb0d13d4e9682ab5043d35cc1
Tasneem <thalim.glam@gmail.com>
2024-03-11 18:17:51 -0700

    Resolving Invalid token error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -24,7 +24,7 @@ module.exports = {
           try {
                const { data } = jwt.verify(token, secret, { maxAge: expiration });
                req.user = data;
-          } catch {
+          } catch (error){
                console.log('Invalid token');
           }
           return req;
