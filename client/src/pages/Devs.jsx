fd10d322cf4c12fa278e3a38dde7a1ba30e1b655
rtocastro <ricktorres@live.com>
2024-03-18 19:24:32 -0700

    aligned cards to one row and added glow

--- client/src/pages/Devs.jsx
+++ client/src/pages/Devs.jsx
@@ -2,6 +2,8 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import devtitle from '../assets/devtitle.png'
 import devRICK from '../assets/devRICK.png'
@@ -15,74 +17,81 @@ export default function Devs() {
     return (
         <>
 
-        <img src={devtitle} />
+            <img src={devtitle} />
 
-            <Card className='devCard'>
-                <Card.Img variant="top" src={devRICK} />
-                <Card.Body>
-                    <Card.Title><strong>Ricardo Torres</strong></Card.Title>
-                    <Card.Subtitle>@RToCastro</Card.Subtitle>
-                    <Card.Text>
-                        Front End Development/REACT/Graphics/CSS
-                    </Card.Text>
-                    <br />
-                    <Link to='https://github.com/rtocastro'>
-                        <Button variant="info">Github</Button>
-                    </Link>
-                    <br />
-                </Card.Body>
-            </Card>
-            
+            <Row>
+                <Col>
+                    <Card className='devCard'>
+                        <Card.Img variant="top" src={devRICK} />
+                        <Card.Body>
+                            <Card.Title><strong>Ricardo Torres</strong></Card.Title>
+                            <Card.Subtitle>@RToCastro</Card.Subtitle>
+                            <Card.Text>
+                                Front End Development/REACT/Graphics/CSS
+                            </Card.Text>
+                            <br />
+                            <Link to='https://github.com/rtocastro'>
+                                <Button variant="info">Github</Button>
+                            </Link>
+                            <br />
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-            <Card className='devCard'>
-                <Card.Img variant="top" src={devVIGGY} />
-                <Card.Body>
-                    <Card.Title><strong>Viggy Sambandam</strong></Card.Title>
-                    <Card.Subtitle>@RToCastro</Card.Subtitle>
-                    <Card.Text>
-                        Back End Development/Javascript/GraphQL/JWT
-                    </Card.Text>
-                    <br />
-                    <Link to='https://github.com/Vigneshwarie'>
-                        <Button variant="info">Github</Button>
-                    </Link>
-                    <br />
-                </Card.Body>
-            </Card>
+                <Col>
+                    <Card className='devCard'>
+                        <Card.Img variant="top" src={devVIGGY} />
+                        <Card.Body>
+                            <Card.Title><strong>Viggy Sambandam</strong></Card.Title>
+                            <Card.Subtitle>@RToCastro</Card.Subtitle>
+                            <Card.Text>
+                                Back End Development/Javascript/GraphQL/JWT
+                            </Card.Text>
+                            <br />
+                            <Link to='https://github.com/Vigneshwarie'>
+                                <Button variant="info">Github</Button>
+                            </Link>
+                            <br />
+                        </Card.Body>
+                    </Card>
+                </Col>
 
+                <Col>
+                    <Card className='devCard'>
+                        <Card.Img variant="top" src={devFRANK} />
+                        <Card.Body>
+                            <Card.Title><strong>Francisco Contreras</strong></Card.Title>
+                            <Card.Subtitle>@RToCastro</Card.Subtitle>
+                            <Card.Text>
+                                Back End Development/Javascript/GraphQL/
+                            </Card.Text>
+                            <br />
+                            <Link to='https://github.com/frankieee324'>
+                                <Button variant="info">Github</Button>
+                            </Link>
+                            <br />
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-            <Card className='devCard'>
-                <Card.Img variant="top" src={devFRANK} />
-                <Card.Body>
-                    <Card.Title><strong>Francisco Contreras</strong></Card.Title>
-                    <Card.Subtitle>@RToCastro</Card.Subtitle>
-                    <Card.Text>
-                    Back End Development/Javascript/GraphQL/
-                    </Card.Text>
-                    <br />
-                    <Link to='https://github.com/frankieee324'>
-                        <Button variant="info">Github</Button>
-                    </Link>
-                    <br />
-                </Card.Body>
-            </Card>
-
-
-            <Card className='devCard'>
-                <Card.Img variant="top" src={devTASNEEM} />
-                <Card.Body>
-                    <Card.Title><strong>Tasneem Halim</strong></Card.Title>
-                    <Card.Subtitle>@RToCastro</Card.Subtitle>
-                    <Card.Text>
-                    Front/Back End Development/Javascript/GraphQL
-                    </Card.Text>
-                    <br />
-                    <Link to='https://github.com/thalim-glam'>
-                        <Button variant="info">Github</Button>
-                    </Link>
-                    <br />
-                </Card.Body>
-            </Card>
+                <Col>
+                    <Card className='devCard'>
+                        <Card.Img variant="top" src={devTASNEEM} />
+                        <Card.Body>
+                            <Card.Title><strong>Tasneem Halim</strong></Card.Title>
+                            <Card.Subtitle>@RToCastro</Card.Subtitle>
+                            <Card.Text>
+                                Front/Back End Development/Javascript/GraphQL
+                            </Card.Text>
+                            <br />
+                            <Link to='https://github.com/thalim-glam'>
+                                <Button variant="info">Github</Button>
+                            </Link>
+                            <br />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
